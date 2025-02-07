85d47d6690ee6c2301bc9cf1b2eeb32de6fbe09d
MightNotTaken <110739834+MightNotTaken@users.noreply.github.com>
2025-02-07 20:16:30 +0530

    removed assests imports

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from util import Camera, Streamer
-from assets import raw_videos
+# from assets import raw_videos
 from time import sleep
 
 
